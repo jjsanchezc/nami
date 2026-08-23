864668182df81007cf4bde2b4ff691d6c0f2fda8
jjsanchezc <jjsanchezc@eafit.edu.co>
2026-08-23 11:51:03 -0500

    fix'

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
 app_path="/opt/finance-tracker"
 
 for i in "$app_path"/data/*; do
-  i=$(rev "$i" | cut "$i" -d / -f 1 | rev)
+  #i=$(rev | cut "$i" -d / -f 1 | rev) # Didn't work using bash,i'll use awk
+  i=$(echo "$i" | awk -F'/' '{print $NF')
   echo "$i"
   new_name="$app_path"/backups/backup_"$(date +"%Y-%m-%d_%H-%M-%S")"_"$i"
   rsync "$i" "$new_name"
