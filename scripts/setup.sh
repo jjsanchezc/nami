3ea3d5749e96c37e8a5601ee74bf712a01efc4af
jjsanchezc <jjsanchezc@eafit.edu.co>
2026-08-24 15:51:58 -0500

    systemd units changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -61,7 +61,7 @@ echo "db.py executed correctly"
 # System units config
 service_path=/etc/systemd/system
 
-cp "$repo_dir"/systemd/ "$service_path"/
+cp -r "$repo_dir"/systemd/ "$service_path"/
 
 systemctl daemon-reload
 # right now is just one service that needs to be activated, but in case that the units grow, think about
